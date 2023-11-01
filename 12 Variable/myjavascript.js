811d6ee0436be82764842afcc7c410781a4d566b
Peyel <mdpeyelalom221@gmail.com>
2023-11-01 23:24:07 +0600

    My_practice_file

---
+++ 12 Variable/myjavascript.js
@@ -0,0 +1,9 @@
+var a = 5;
+var b = 6;
+var c = a+b;
+console.log(c);
+var firstName = 'Peyel';
+var lastName = 'Islam';
+var age = 22;
+console.log(firstName,"      ",lastName, "is",age,"years old");
+
