7fec829c710a02b29c4fd8334407f916430576f3
Peyel <mdpeyelalom221@gmail.com>
2023-11-02 22:22:51 +0600

    My_practice_file

---
+++ 22 Conditional statement/myjavascript.js
@@ -0,0 +1,20 @@
+// var a = 30;
+// var b = 20;
+
+// if ( a<b ){
+//     console.log('a is bigger');
+//     console.log('I love Bangladesh');
+// }
+var taka = 30;
+var coupon = 1;
+if ( taka >= 100 ){
+    console.log('YES! You can buy a car');
+}
+else if( coupon = 1 ){
+    console.log('YES! You can buy a car with your coupon code.');
+
+}
+else{
+    console.log('No! You can not buy a car');
+    
+}
